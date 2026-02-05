0b644779aa5fdbb20dae371376fb9a8cb2e17268
mtothejensen <moje1000@stud.ek.dk>
2026-02-05 10:51:55 +0100

    adding a second ride

--- src/TivoliRide.java
+++ src/TivoliRide.java
@@ -3,17 +3,32 @@ public class TivoliRide {
 
     int height;
 
-    public TivoliRide(int height) {
+    public TivoliRide1(int height) {
         this.height = height;
 
     }
 
-    public String getTivoliRide(){
+    public String getTivoliRide1(){
 
         if (height >= 160) {
-            return "Welcome to the ride weeeee";
+            return "Welcome to the ride in our car";
         } else {
-            return "You're too low homie, see ya";
+            return "You're too low for cars homie, see ya";
+        }
+
+    }
+
+    public TivoliRide2(int height) {
+        this.height = height;
+
+    }
+
+    public String getTivoliRide2(){
+
+        if (height >= 20) {
+            return "Welcome to the bike ride weeeee";
+        } else {
+            return "You're too low for bikes (what??) bye bye";
         }
 
     }
