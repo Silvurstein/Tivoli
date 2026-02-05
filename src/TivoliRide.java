20ac558bf5e7e6238356e3a71718a893f51b307d
mtothejensen <moje1000@stud.ek.dk>
2026-02-05 10:35:52 +0100

    first try

--- src/TivoliRide.java
+++ src/TivoliRide.java
@@ -1,6 +1,21 @@
 
 public class TivoliRide {
 
-    //MJ SKRIVER
+    int height;
+
+    public TivoliRide(int height) {
+        this.height = height;
+
+    }
+
+    public String getTivoliRide(){
+
+        if (height >= 160) {
+            return "Welcome to the ride weeeee";
+        } else {
+            return "You're too low homie, see ya";
+        }
+
+    }
 
 }
