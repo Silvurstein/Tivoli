d9cb980705c973894c713131c1254bfe56ee6fe1
silvurstein <sveinursilvurstein@gmail.com>
2026-02-05 10:44:17 +0100

    Test 2

--- src/TivoliRideTest.java
+++ src/TivoliRideTest.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 
 public class TivoliRideTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    //hej?
+        System.out.println("Enter you height: ");
+        int height = sc.nextInt();
+
+        TivoliRide ride = new TivoliRide(height);
+
+        System.out.println(ride.getTivoliRide());
+
+        sc.close();
+
+    }
 }
